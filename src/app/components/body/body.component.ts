12d14c313fbcd3c94914a94a2cda402e6b9aefbd
Cristian Bermedo <cristianbermedog@gmail.com>
2018-08-02 01:45:47 -0400

    add *ngIf directive

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -4,7 +4,14 @@ import { Component } from '@angular/core';
     selector: 'app-body',
     templateUrl: './body.component.html'
 })
-
 export class BodyComponent {
 
+    state = true;
+
+    phrase: any = {
+        author: 'Woody Allen',
+        message: 'No es que tenga miedo de morir, pero no quiero estar allí cuando ocurra'
+    };
+
+    changeState = () => this.state = !this.state;
 }
